df0d4d20b9608d5ae63336f3f3dbc6b0107b162d
Filadelfo Braz <filadelfo.braz@gmail.com>
2019-07-30 14:39:10 -0400

    responding as a json obj

--- src/server/api/index.js
+++ src/server/api/index.js
@@ -6,10 +6,22 @@ const upload = multer()
 
 router.post('/upload', upload.single('file'), (req, res, next) => {
   const { file } = req
-  console.log(file)
   const fileToString = file.buffer.toString()
-  console.log(fileToString)
-  res.send('done')
+  const fileToJSON = []
+  const fileToArray = fileToString.split('\n')
+  const header = fileToArray[0].split(',')
+  const onlyDigits = new RegExp('^\\d+$')
+  for(let i = 1; i < fileToArray.length; i++) {
+    let newEntry = {}
+    let arrSingleEntry = fileToArray[i].split(',')
+    for(let j = 0; j < header.length; j++) {
+      let value = arrSingleEntry[j].replace('\r','')
+      newEntry[header[j]] = onlyDigits.test(value) ? Number(value) : value
+    }
+    fileToJSON.push(newEntry)
+  }
+  console.log(fileToJSON)
+  res.json(fileToJSON)
 })
 
 export default router
