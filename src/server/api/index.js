4e47d307346bb1157414ed878bf4271fb0f8ccb0
Filadelfo Braz <filadelfo.braz@gmail.com>
2019-07-30 17:28:10 -0400

    fixed data transformation to accept float as number

--- src/server/api/index.js
+++ src/server/api/index.js
@@ -10,17 +10,16 @@ router.post('/upload', upload.single('file'), (req, res, next) => {
   const fileToJSON = []
   const fileToArray = fileToString.split('\n')
   const header = fileToArray[0].split(',')
-  const onlyDigits = new RegExp('^\\d+$')
+  const onlyDigits = new RegExp('^[-+]?[0-9]*\.?[0-9]+$')
   for(let i = 1; i < fileToArray.length; i++) {
     let newEntry = {}
     let arrSingleEntry = fileToArray[i].split(',')
     for(let j = 0; j < header.length; j++) {
-      let value = arrSingleEntry[j].replace('\r','')
+      let value = arrSingleEntry[j] ? arrSingleEntry[j].replace('\r','') : null
       newEntry[header[j]] = onlyDigits.test(value) ? Number(value) : value
     }
     fileToJSON.push(newEntry)
   }
-  console.log(fileToJSON)
   res.json(fileToJSON)
 })
 
