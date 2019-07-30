6aeddb61130fd06ea640de7f6a61ccda1dd2f9e5
Filadelfo Braz <filadelfo.braz@gmail.com>
2019-07-29 22:18:30 -0400

    able to transform buffer to string

--- src/server/api/index.js
+++ src/server/api/index.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import multer from 'multer'
+import fs from 'fs'
 const router = express.Router()
 const upload = multer()
 
 router.post('/upload', upload.single('file'), (req, res, next) => {
   const { file } = req
   console.log(file)
+  const fileToString = file.buffer.toString()
+  console.log(fileToString)
   res.send('done')
 })
 
